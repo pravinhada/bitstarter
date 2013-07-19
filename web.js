363c0638c09abfa0932eaee5c66a131766ec1db6
Pravin Hada <pravin_hada@yahoo.com>
2013-07-19 00:43:19 +0000

    Updating all the files.

--- web.js
+++ web.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var fs = require('fs');
+var htmlFile = "index.html";
 
 var app = express.createServer(express.logger());
 
@@ -9,7 +10,7 @@ app.get('/', function(request, response) {
     response.send(content);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
